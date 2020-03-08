407b263241e7429b32d2ca6de66fc30bc10740cf
Julian Van Ost <julian.vanost@gmail.com>
2020-03-08 16:17:44 -0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,12 @@
 const { createConnection } = require('mysql2')
 
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = createConnection(process.env.JAWSDB_URL)
+}
 const connection = createConnection({
-  host: 'tyduzbv3ggpf15sx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	',
+  host: 'tyduzbv3ggpf15sx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   port: 3306,
   user: 'ps31oim3lpv7m34r',
   password: 'cusw0t18caynbsc6',
