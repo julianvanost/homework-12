357b87e91d745abeb2fd53d5f17da1abc92650c0
Julian <Van Ost>
2020-03-08 13:48:43 -0700

    Adds all necessary files and updates readme.md with instructions

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@ const { createConnection } = require('mysql2')
 const connection = createConnection({
   host: 'localhost',
   port: 3306,
-  user: 'Account',
-  password: 'Fight!Me1',
+  user: 'root',
+  password: 'rootroot',
   database: 'employees_db'
 })
 
